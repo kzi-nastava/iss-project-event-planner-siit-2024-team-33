6f88653657e57babd9eaf55f904b31cd26ddf83a
MikroMirza <mirkodjukic314@gmail.com>
2024-11-11 12:24:45 +0100

    Added server part of the project

---
+++ ISS/Projekatsiit2024/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2024/Projekatsiit2024Application.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekatsiit2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekatsiit2024Application.class, args);
+	}
+
+}
