0a338285bdfa0d2dd654694369c0b6995cca9aba
MikroMirza <mirkodjukic314@gmail.com>
2024-12-02 18:45:00 -0800

    [FIX] fixed some minor issues so I don't forget tomorrow morning (in about 2 horus)

---
+++ ISS/Projekatsiit2024/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2024/dto/service/MinimalServiceDTO.java
@@ -0,0 +1,10 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2024.dto.service;
+
+import java.util.List;
+
+public class MinimalServiceDTO {
+	public String name;
+	public Double basePrice;
+	public String description;
+	public String images;
+}
