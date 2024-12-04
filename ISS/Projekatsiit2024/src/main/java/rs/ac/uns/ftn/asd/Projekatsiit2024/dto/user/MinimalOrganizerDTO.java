278b57b822b676490e5a42a9037aea850edda493
MikroMirza <mirkodjukic314@gmail.com>
2024-12-04 01:05:54 +0100

    .

---
+++ ISS/Projekatsiit2024/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2024/dto/user/MinimalOrganizerDTO.java
@@ -0,0 +1,15 @@
+package rs.ac.uns.ftn.asd.Projekatsiit2024.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MinimalOrganizerDTO {
+	public int id;
+	public String name;
+	public String surname;
+	public String picture;
+	public String residency;
+	public String phoneNumber;
+}
