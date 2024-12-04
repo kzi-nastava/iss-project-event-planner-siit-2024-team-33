f21b6197a0a387b3b4f987aaab17f684433a88eb
MilicMilosRS <milicmilos.rs@gmail.com>
2024-12-04 13:23:08 +0100

    Merge branch 'Develop' of git@github.com:kzi-nastava/iss-project-event-planner-siit-2024-team-33.git into Develop

--- ISS/Projekatsiit2024/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2024/Repository/ServiceRepository.java
+++ ISS/Projekatsiit2024/src/main/java/rs/ac/uns/ftn/asd/Projekatsiit2024/Repository/ServiceRepository.java
@@ -14,4 +14,7 @@ public interface ServiceRepository extends JpaRepository<Service, Integer>{
 	
 	@Query("SELECT s FROM Service s WHERE s.id=(SELECT max(s2.id) FROM Service s2 WHERE s2.offerID=:offerId)")
 	public Service getLatestServiceVersion(@Param("offerId") Integer offerId);
+	
+	@Query("SELECT s FROM Service s WHERE s.id=:versionId AND s.offerID=:offerId")
+	public Service findServiceWithVersion(@Param("offerId") Integer offerId, @Param("versionId") Integer versionId);
 }
