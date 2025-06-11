cece1992c5fd92005a438955316097adbe84aff9
MikroMirza <mirkodjukic314@gmail.com>
2025-06-11 16:06:43 +0200

    GAS

--- ISS/Projekatsiit2024/src/main/resources/data.sql
+++ ISS/Projekatsiit2024/src/main/resources/data.sql
@@ -30,15 +30,38 @@ VALUES
 (TRUE, TRUE, 'Best service', 'Catering'),
 (TRUE, TRUE, 'Delicious food', 'Food');
 
-INSERT INTO OFFER (CATEGORY_ID, CREATION_DATE, DISCOUNT, IS_DELETED, IS_PENDING, OFFERID, PRICE, PROVIDER_ID, TYPE, DTYPE, DESCRIPTION, NAME, AVAILABILITY, PICTURES,CITY)
+-- Insert PRODUCT offers
+INSERT INTO OFFER (
+    CATEGORY_ID, CREATION_DATE, DISCOUNT, IS_DELETED, IS_PENDING, 
+    OFFERID, PRICE, PROVIDER_ID, TYPE, DTYPE, DESCRIPTION, NAME, 
+    AVAILABILITY, PICTURES, CITY
+)
 VALUES
-(1, '2024-12-01', 10.0, FALSE, FALSE, 1, 49.99, 1, 'PRODUCT', 'Product', 'Great chinese quality speaker', 'JBL Speaker', 'AVAILABLE', ARRAY['jbl.jpg'],'Paris'),
-(2, '2024-11-30', 15.5, FALSE, FALSE, 2, 29.99, 2, 'PRODUCT', 'Product', 'Beautiful silver plates that will make you mega happy', 'Silver plates', 'AVAILABLE', 'image2.png','Paris'),
-(3, '2024-10-15', 20.0, FALSE, FALSE, 3, 79.99, 3, 'PRODUCT', 'Product', 'Beautiful sour yummy candeis', 'Candy', 'AVAILABLE', 'image3.jpg','Paris');
+(1, '2024-12-01', 10.0, FALSE, FALSE, 1, 49.99, 1, 'PRODUCT', 'Product', 'Great chinese quality speaker', 'JBL Speaker', 'AVAILABLE', ARRAY['jbl.jpg'], 'Paris'),
+(2, '2024-11-30', 15.5, FALSE, FALSE, 2, 29.99, 1, 'PRODUCT', 'Product', 'Beautiful silver plates that will make you mega happy', 'Silver plates', 'AVAILABLE', ARRAY['image2.png'], 'Paris'),
+(3, '2024-10-15', 20.0, FALSE, FALSE, 3, 79.99, 1, 'PRODUCT', 'Product', 'Beautiful sour yummy candies', 'Candy', 'AVAILABLE', ARRAY['image3.jpg'], 'Paris');
 
-INSERT INTO OFFER (CANCELLATION_IN_HOURS, CATEGORY_ID, DISCOUNT, IS_AUTOMATIC, IS_DELETED, IS_PENDING, MAX_LENGTH_IN_MINS, MIN_LENGTH_IN_MINS, OFFERID, PRICE, PROVIDER_ID, RESERVATION_IN_HOURS, CREATION_DATE, DTYPE, DESCRIPTION, NAME, AVAILABILITY, PICTURES, TYPE, CITY)
-VALUES
-(24, 1, 10.0, TRUE, FALSE, FALSE, 120, 5, 4, 99.99, 2, 48, '2024-12-01', 'Service', 'overrated norwegian black metal lol Burzum (/ˈbɜːrzəm/; Norwegian: [ˈbʉ̀rtsʉm]) is a Norwegian music project founded by Varg Vikernes in 1991. Although Burzum never played live performances, it became a staple of the early Norwegian black metal scene and is considered one of the most influential acts in black metal`s history.', 'BURZUM', 'AVAILABLE', ARRAY['zumzum.jpg', 'burzum.jpg'], 'SERVICE', 'New York ');
+-- Insert SERVICE offer
+INSERT INTO OFFER (
+    CANCELLATION_IN_HOURS, CATEGORY_ID, DISCOUNT, IS_AUTOMATIC, IS_DELETED, IS_PENDING, 
+    MAX_LENGTH_IN_MINS, MIN_LENGTH_IN_MINS, OFFERID, PRICE, PROVIDER_ID, 
+    RESERVATION_IN_HOURS, CREATION_DATE, DTYPE, DESCRIPTION, NAME, 
+    AVAILABILITY, PICTURES, TYPE, CITY
+)
+VALUES (
+    24, 1, 10.0, TRUE, FALSE, FALSE, 
+    120, 5, 4, 99.99, 1, 
+    48, '2024-12-01', 'Service', 
+    'Overrated Norwegian black metal. Burzum (/ˈbɜːrzəm/; Norwegian: [ˈbʉ̀rtsʉm]) is a Norwegian music project founded by Varg Vikernes in 1991. Although Burzum never played live performances, it became a staple of the early Norwegian black metal scene and is considered one of the most influential acts in black metal''s history.',
+    'BURZUM', 'AVAILABLE', ARRAY['zumzum.jpg', 'burzum.jpg'], 'SERVICE', 'New York'
+);
+VALUES (
+    24, 1, 10.0, TRUE, FALSE, FALSE, 
+    120, 5, 4, 9.99, 1, 
+    48, '2024-12-01', 'Service', 
+    'Overrated Norwegian black metal. Burzum (/ˈbɜːrzəm/; Norwegian: [ˈbʉ̀rtsʉm]) is a Norwegian music project founded by Varg Vikernes in 1991. Although Burzum never played live performances, it became a staple of the early Norwegian black metal scene and is considered one of the most influential acts in black metal''s history.',
+    'BURZUM', 'AVAILABLE', ARRAY['zumzum.jpg', 'burzum.jpg'], 'SERVICE', 'New York'
+);
 
 
 INSERT INTO EVENT_TYPE(IS_ACTIVE, DESCRIPTION, NAME) VALUES
@@ -97,10 +120,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -112,10 +135,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -127,10 +150,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -142,10 +165,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -157,10 +180,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -172,10 +195,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -187,10 +210,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -202,10 +225,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -217,10 +240,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -232,10 +255,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -247,10 +270,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -262,10 +285,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -277,10 +300,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -292,10 +315,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -307,10 +330,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -322,25 +345,10 @@ sdfklsdfklsdlfkslkfklslkdfklsdf',
     150, 
     5, 
     50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
-),
-(
-    CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
-    CAST('2024-12-15 19:00:00' AS TIMESTAMP), 
-    0, 
-    0, 
-    40.712776, 
-    -74.005974, 
-    150, 
-    5, 
-    50, 
-    'Neam pojma nesto kenjam kao neka jaka deskripcija', 
-    'Alo najjaca zureza ikadas dfdskfdsk', 
-    'event1.jpg', 
-    'Jew York City'
+    'Annual Winter Gala', 
+    'Winter Wonderland', 
+    'event2.jpg', 
+    'New York City'
 ),
 (
     CAST('2024-12-15 07:00:00' AS TIMESTAMP), 
@@ -437,5 +445,3 @@ INSERT INTO report (content, date_of_sending, author_id, receiver_id) VALUES
 ('Feedback on the event organization.', '2024-12-07', 1, 3),
 ('Summary of the last quarter performance.', '2024-12-08', 2, 3),
 ('Report concerning the new project updates.', '2024-12-09', 3, 1);
-
-
